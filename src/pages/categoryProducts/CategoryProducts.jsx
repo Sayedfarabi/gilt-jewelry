0ca2e96dba60e88aa81c2d79cbb923c27e69a895
Sayedfarabi <sayedfarabi6099@gmail.com>
2023-04-07 21:03:22 +0600

    favicon and title chonge done

--- src/pages/categoryProducts/CategoryProducts.jsx
+++ src/pages/categoryProducts/CategoryProducts.jsx
@@ -31,7 +31,7 @@ const CategoryProducts = () => {
             <div className='py-8 md:py-12'>
                 <h1 className='text-center text-primary text-2xl md:text-4xl'> PRODUCTS</h1>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-3 py-8'>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-4 py-8'>
                 {
                     products &&
                     categoryProducts?.map(product => {
